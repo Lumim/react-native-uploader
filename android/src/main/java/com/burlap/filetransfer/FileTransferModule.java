c4cfb9b0ddccb07b05144cbfc010f23fde268a4a
Ryan Vanbelkum <ryan.vanbelkum@equipmentshare.com>
2018-01-23 12:10:30 -0600

    android file compression

--- android/src/main/java/com/burlap/filetransfer/FileTransferModule.java
+++ android/src/main/java/com/burlap/filetransfer/FileTransferModule.java
@@ -2,6 +2,8 @@ package com.burlap.filetransfer;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -20,6 +22,8 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.zhy.http.okhttp.request.CountingRequestBody;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
@@ -81,8 +85,9 @@ public class FileTransferModule extends ReactContextBaseJavaModule {
                     }
 
                     File imageFile = new File(file_uri.getPath());
+                    File compressedFile = saveBitmapToFile(imageFile);
 
-                    if (imageFile == null) {
+                    if (compressedFile == null) {
                         Log.d(TAG, "FILE NOT FOUND");
                         completeCallback.invoke("FILE NOT FOUND", null);
                         return;
@@ -100,7 +105,7 @@ public class FileTransferModule extends ReactContextBaseJavaModule {
                     }
 
 
-                    mRequestBody.addFormDataPart(name, imageFile.getName(), RequestBody.create(mediaType, imageFile));
+                    mRequestBody.addFormDataPart(name, compressedFile.getName(), RequestBody.create(mediaType, compressedFile));
                 }
             }
 
@@ -153,7 +158,7 @@ public class FileTransferModule extends ReactContextBaseJavaModule {
         }
     }
 
-    public String convertMediaUriToPath(Uri uri) {
+    private String convertMediaUriToPath(Uri uri) {
         Context context = getReactApplicationContext();
         String[] proj = {MediaStore.Images.Media.DATA};
         Cursor cursor = context.getContentResolver().query(uri, proj, null, null, null);
@@ -162,5 +167,48 @@ public class FileTransferModule extends ReactContextBaseJavaModule {
         String path = cursor.getString(column_index);
         cursor.close();
         return path;
+    }
+
+    private File saveBitmapToFile(File file){
+        try {
+
+            // BitmapFactory options to downsize the image
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            o.inSampleSize = 6;
+            // factor of downsizing the image
+
+            FileInputStream inputStream = new FileInputStream(file);
+            //Bitmap selectedBitmap = null;
+            BitmapFactory.decodeStream(inputStream, null, o);
+            inputStream.close();
+
+            // The new size we want to scale to
+            final int REQUIRED_SIZE=75;
+
+            // Find the correct scale value. It should be the power of 2.
+            int scale = 1;
+            while(o.outWidth / scale / 2 >= REQUIRED_SIZE &&
+                    o.outHeight / scale / 2 >= REQUIRED_SIZE) {
+                scale *= 2;
+            }
+
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = scale;
+            inputStream = new FileInputStream(file);
+
+            Bitmap selectedBitmap = BitmapFactory.decodeStream(inputStream, null, o2);
+            inputStream.close();
+
+            // here i override the original image file
+            file.createNewFile();
+            FileOutputStream outputStream = new FileOutputStream(file);
+
+            selectedBitmap.compress(Bitmap.CompressFormat.JPEG, 75 , outputStream);
+
+            return file;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
